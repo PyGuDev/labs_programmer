3950389eccca946e83e400501dd5752b6d42707d
PyGuDev <gubaev1999@gmail.com>
2021-01-16 14:57:42 +0500

    Доработка программы

--- laba3/create_team.py
+++ laba3/create_team.py
@@ -4,12 +4,17 @@ import random
 
 
 class CreateTeamWidget(QW.QWidget):
+    """
+    Окно созданием команды
+    Задается кол-во игроков
+    """
     def __init__(self):
         QW.QWidget.__init__(self)
         uic.loadUi('create_teams.ui', self)
-        self.pushButton.clicked.connect(self.creaet_players)
+        self.pushButton.clicked.connect(self.next_widget)
 
-    def creaet_players(self):
+    def next_widget(self):
+        # Переход на другое окно
         self.count_player = int(self.lineEdit.text())
         save_player_widget = SavePlayerWidget(self.count_player)
         save_player_widget.show()
@@ -17,25 +22,39 @@ class CreateTeamWidget(QW.QWidget):
 
 
 class SavePlayerWidget(QW.QWidget):
+    """
+    Окно генерация игроков
+    """
     def __init__(self, count_player):
         QW.QWidget.__init__(self)
         uic.loadUi('players.ui', self)
         self.count_player = count_player
         self.players_layout = QW.QVBoxLayout()
+        self.size_shoe_players = []
         for i in range(self.count_player):
             self.players_layout.addLayout(PlayerWidget())
         self.setLayout(self.players_layout)
         self.btn_search_shoe_in_shop = QW.QPushButton('Искать обувь в мазине')
-        self.btn_search_shoe_in_shop.clicked.connect(self.search_shoe_in_shop)
+        self.btn_search_shoe_in_shop.clicked.connect(self.next_widget)
         self.players_layout.addWidget(self.btn_search_shoe_in_shop)
 
-    def search_shoe_in_shop(self):
-        self.shop = CreateShoeShop()
+    def get_shoes_all_player(self):
+        # Получение размера всех игрково
+        widgets = self.players_layout.findChildren(PlayerWidget)
+        self.size_shoe_players = [widget.get_shoe_size() for widget in widgets]
+        return self.size_shoe_players
+
+    def next_widget(self):
+        # Переход на другое окно
+        self.shop = CreateShoeShop(self.get_shoes_all_player())
         self.shop.show()
         self.close()
 
 
 class PlayerWidget(QW.QHBoxLayout):
+    """
+    Виджет игрока
+    """
     def __init__(self):
         QW.QHBoxLayout.__init__(self)
         self.input_name = QW.QLineEdit()
@@ -46,23 +65,42 @@ class PlayerWidget(QW.QHBoxLayout):
         self.addWidget(self.input_shoe_size)
 
     def get_shoe_size(self):
-        return self.input_shoe_size.text()
+        return int(self.input_shoe_size.text())
 
 
 class CreateShoeShop(QW.QWidget):
-    def __init__(self):
+    """
+    Окно создания магазина
+    """
+    def __init__(self, shoe_size_all_players):
         QW.QWidget.__init__(self)
         uic.loadUi('create_shop.ui', self)
         self.add_shoe.clicked.connect(self.generate_shoe)
-        self.shoes = []
+        self.shoe_size_all_players = shoe_size_all_players
+        self.shoes_size = []
 
     def generate_shoe(self):
+        # Рандомная генерация игрока
         self.count_shoe = int(self.input_count_shoe.text())
-        self.shoes = [random.randint(39, 45) for i in range(self.count_shoe)]
+        self.shoes_size = [random.randint(39, 45) for i in range(self.count_shoe)]
+        count_curent_shoe = 0
+        for shoe_size in self.shoes_size:
+            if shoe_size in self.shoe_size_all_players:
+                count_curent_shoe += 1
+        self.result_widget = ResultWidget(self.shoes_size.__len__(), count_curent_shoe)
+        self.result_widget.show()
+        self.close()
 
-    def get_shoes(self):
-        return self.count_shoe
 
+class ResultWidget(QW.QWidget):
+    """
+    Окно вывода резултата
+    """
+    def __init__(self, count_shoe, count_curent_shoe):
+        QW.QWidget.__init__(self)
+        uic.loadUi('result.ui', self)
+        self.count_shoe.setText(str(count_shoe))
+        self.count_curent_shoe.setText(str(count_curent_shoe))
 
 
 if __name__ == "__main__":
